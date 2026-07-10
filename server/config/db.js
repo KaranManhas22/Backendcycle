55751edc2c2d10d80ca188e710930c8dfc93e2be
Karan Singh Manhas <karansingh33477@gmail.com>
2026-07-10 13:12:50 +0530

    commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 let mongoose = require('mongoose')
-const dbPath = 'mongodb+srv://localhost/bicycleDb'
+const dbPath = 'mongodb+srv://karansingh33477:22May2004@cluster0.1xposgq.mongodb.net/bicycleDb'
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true }
 mongoose.connect(dbPath, options).then(res => {
