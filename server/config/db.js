38106dff9a02cd31df5cae9ca0cfe63337698db5
Karan Singh Manhas <karansingh33477@gmail.com>
2026-07-10 12:52:25 +0530

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+let mongoose = require('mongoose')
+const dbPath = 'mongodb+srv://localhost/bicycleDb'
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(dbPath, options).then(res => {
+    console.log("Db Connected")
+}).catch(err => {
+    console.log("Db Connect Err", err)
+})
